3df3ad34dda510af28ab76dd281debf1d362acc1
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-06-19 06:46:00 +0000

    Adjusted after removal of automated import of subpackages

--- trunk/examples/IO.ANDI.Class.Chemstation/proc.py
+++ trunk/examples/IO.ANDI.Class.Chemstation/proc.py
@@ -4,17 +4,17 @@
 import sys
 sys.path.append("/home/current/proj/PyMS/")
 
-from pyms import IO 
-from pyms import Utils 
+from pyms.IO.ANDI.Class import ChemStation 
+from pyms.Utils.IO import save_data 
 
 andi_file = "/home/current/proj/PyMS/pyms-data/0510_217.CDF"
 
-data = IO.ANDI.Class.ChemStation(andi_file)
+data = ChemStation(andi_file)
 
 im = data.get_intensity_matrix()
 ic = data.get_tic()
 
-Utils.IO.save_data("output/im.dat", im)
+save_data("output/im.dat", im)
 
 ic.write("output/ticm.dat", minutes=True)
 ic.write("output/tics.dat", minutes=False)
