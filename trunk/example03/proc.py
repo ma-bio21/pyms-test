1085ed1f97a9dbd3dab60bc8547b79e6790e8f19
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-08-01 06:22:08 +0000

    Updated example03

--- trunk/example03/proc.py
+++ trunk/example03/proc.py
@@ -13,11 +13,17 @@ andi_file = "/home/current/proj/PyMS/pyms-data/0510_217.CDF"
 # read the ANDI-MS data
 data = ChemStation(andi_file)
 
+# get the TIC and save to a file
+ic = data.get_tic()
+ic.write("output/tic.dat")
+
 # get the intensity matrix and save to a file
 im = data.get_intensity_matrix()
 save_data("output/im.dat", im)
 
-# get the TIC and save to a file
-ic = data.get_tic()
-ic.write("output/tic.dat")
+# export the entire data as CSV. This will create
+# data.im.csv, data.mz.csv, and data.rt.csv where
+# these are the intensity matrix, retention time
+# vector, and m/z vector in the CSV format
+data.export_csv("output/data")
 
