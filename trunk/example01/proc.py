96ba0f87332ca10ceb979ab2114a8bd3b18dd2fc
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-08-01 03:28:17 +0000

    Developed example02

--- trunk/example01/proc.py
+++ trunk/example01/proc.py
@@ -5,17 +5,8 @@ import sys
 sys.path.append("/home/current/proj/PyMS/")
 
 from pyms.IO.ANDI.Class import ChemStation
-from pyms.Utils.IO import save_data
 
 andi_file = "/home/current/proj/PyMS/pyms-data/0510_217.CDF"
 
 data = ChemStation(andi_file)
-
-im = data.get_intensity_matrix()
-ic = data.get_tic()
-
-save_data("output/im.dat", im)
-
-ic.write("output/ticm.dat", minutes=True)
-ic.write("output/tics.dat", minutes=False)
 
---
+++ trunk/example02/proc.py
@@ -0,0 +1,32 @@
+"""proc.py
+"""
+
+import sys
+sys.path.append("/home/current/proj/PyMS/")
+
+from pyms.IO.ANDI.Class import ChemStation
+
+andi_file = "/home/current/proj/PyMS/pyms-data/0510_217.CDF"
+
+data = ChemStation(andi_file)
+
+# print the name of the ANDI-MS file
+print data.get_filename()
+
+# get the first ion chromatogram in the file
+ic = data.get_ic_at_index(1)
+print ic._mass
+
+# get TIC
+tic = data.get_tic()
+print tic._mass
+
+# get the ion chromatogram for m/z = 73
+ic = data.get_ic_at_mass(73)
+print ic._mass
+
+# get the entire intensity matrix
+im = data.get_intensity_matrix()
+print len(im)
+print len(im[0])
+
