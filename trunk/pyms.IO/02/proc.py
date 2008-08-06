5410f3d7e3bccb105abe0efce730bb389571290f
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-08-06 09:10:15 +0000

    Consolidating pyms.IO examples

--- trunk/pyms.IO/02/proc.py
+++ trunk/pyms.IO/02/proc.py
@@ -13,22 +13,34 @@ andi_file = "/home/current/proj/PyMS/pyms-data/0510_217.CDF"
 data = ChemStation(andi_file)
 
 # print the name of the ANDI-MS file
-print data.get_filename()
-
-# get the first ion chromatogram in the file
-ic = data.get_ic_at_index(1)
-print ic.is_tic()
+print "ANDI-MS data filename:", data.get_filename()
 
 # get TIC
 tic = data.get_tic()
-print tic.is_tic()
 
+# get the first ion chromatogram
+ic = data.get_ic_at_index(1)
 # get the ion chromatogram for m/z = 73
 ic = data.get_ic_at_mass(73)
-print ic.is_tic()
+
+# some tests on ion chromatogram objects
+print "'tic' is a TIC", tic.is_tic()
+print "'ic' is a TIC:",ic.is_tic()
+
+# save TIC to a file
+ic.write("output/tic.dat")
 
 # get the entire intensity matrix
 im = data.get_intensity_matrix()
-print len(im)
-print len(im[0])
+
+print "Dimensions of the intensity matrix are:",len(im),"x",len(im[0])
+
+# save the intensity matrix  to a file
+save_data("output/im.dat", im)
+
+# Export the entire data as CSV. This will create
+# data.im.csv, data.mz.csv, and data.rt.csv where
+# these are the intensity matrix, retention time
+# vector, and m/z vector in the CSV format
+data.export_csv("output/data")
 
