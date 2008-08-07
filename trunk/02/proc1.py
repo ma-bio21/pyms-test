7afc2bb302d241fcd6a4b080ca7daa05a814b667
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-08-07 07:31:38 +0000

    Example development

--- trunk/02/proc1.py
+++ trunk/02/proc1.py
@@ -5,11 +5,13 @@ import sys
 sys.path.append("/home/current/proj/PyMS/")
 
 from pyms.Peak.Class import Peak
-p = Peak(360.0,123337)
 
+# create a peak with retention time 360.0 s and raw area of 123337.0
+p = Peak(5.553*60.0,2759280)
+
+# print some properties of this peak
 print "Peak retention time is", p.rt
 print "Peak raw area is", p.raw_area
 print "Peak normalized area is", p.norm_area
 print "Peak mass spectrum is", p.mass_spectrum
-
 
--- trunk/02/proc2.py
+++ trunk/02/proc2.py
@@ -9,21 +9,21 @@ from pyms.Experiment.Class import Experiment
 from pyms.Peak.List.IO import read_chem_station_peaks
 from pyms.Experiment.IO import dump_expr
 
-# path to ANDI-MS data file
+# path to ANDI-MS data file and  annotated ChemStation peak file
 andi_file = "/home/current/proj/PyMS/pyms-data/a0806_140.CDF"
-# path to annotated ChemStation peak file
 peak_file = "/home/current/proj/PyMS/pyms-data/a0806_140.txt.anno"
 
 # read the ANDI-MS data file and Chemstation peak report
 andi_data = ChemStation(andi_file)
-
 peaks = read_chem_station_peaks(peak_file)
 
-# set the mass spectrum for each peak
+# set the mass spectrum for each peak, and crop mass spectrum
+# to mz in range 50-540
 for peak in peaks:
     peak.set_mass_spectrum(andi_data)
     peak.crop_mass_spectrum(50, 540)
 
+# set up an experiment from this set of peaks
 expr = Experiment("a0806_140", peaks)
 expr.set_ref_peak("si")
 expr.remove_blank_peaks()
@@ -31,5 +31,6 @@ expr.raw2norm_area()
 expr.purge_peaks()
 expr.sele_rt_range(["6.5m", "21m"])
 
-dump_expr(expr, "output/" + expr.expr_code + ".pickle")
+# dump experiment to a file
+dump_expr(expr, "output/a0806_140.pickle")
 
