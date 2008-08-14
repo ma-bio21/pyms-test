a1c0da3e397e2115a874e363def19b02f016b4f5
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-08-14 09:03:02 +0000

    Added example 11

---
+++ trunk/11/proc.py
@@ -0,0 +1,23 @@
+"""proc.py
+"""
+
+import sys
+sys.path.append("/home/current/proj/PyMS/")
+
+from pyms.IO.ANDI.Class import ChemStation
+from pyms.Noise.Window import window_smooth
+
+# path to ANDI-MS data file
+andi_file = "/home/current/proj/PyMS/pyms-data/a0806_140.CDF"
+
+# get the intensity matrix and save to a file
+andi_data = ChemStation(andi_file)
+
+# get TIC
+tic = andi_data.get_tic()
+
+# smooth TIC in two ways
+tic1 = window_smooth(tic, window=3)
+tic2 = window_smooth(tic, window=3, median=True)
+
+
