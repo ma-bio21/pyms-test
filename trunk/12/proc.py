1ed48bbde92026060f83aff21cccae70ea924a54
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-08-15 09:55:24 +0000

    Added example 12- using tophat basline corrector

---
+++ trunk/12/proc.py
@@ -0,0 +1,25 @@
+"""proc.py
+"""
+
+import sys
+sys.path.append("/home/current/proj/PyMS/")
+
+from pyms.IO.ANDI.Class import ChemStation
+from pyms.Noise.Window import window_smooth
+from pyms.Baseline.TopHat import tophat
+
+# load the data
+andi_file = "/home/current/proj/PyMS/pyms-data/a0806_140.CDF"
+andi_data = ChemStation(andi_file)
+
+# get the TIC
+tic = andi_data.get_tic()
+
+# apply 5-point moving window smoothing & baseline corrector
+tic = window_smooth(tic, window=5)
+tic2 = tophat(tic, struct="1m")
+
+# save the original and processed TICs
+tic.write("output/tic.dat",minutes=True)
+tic2.write("output/tic2.dat",minutes=True)
+
