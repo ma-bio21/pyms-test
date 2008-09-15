4b26f936c1670cbe3631ad1e70dbc7fa3b05618a
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-09-15 01:12:04 +0000

    Example for the sub-package Simulator. At present in development

---
+++ trunk/41/proc.py
@@ -0,0 +1,18 @@
+"""proc.py
+"""
+
+import sys
+sys.path.append("/home/current/proj/PyMS/")
+
+from pyms.Simulator.Class import GCMS_simulator
+
+s = GCMS_simulator(rt=['5.1m','21m',0.343], mz=[51,550,1])
+
+rt = s.get_time_list()
+mz = s.get_mass_list()
+im = s.get_intensity_matrix()
+
+print rt
+print mz
+print im
+
