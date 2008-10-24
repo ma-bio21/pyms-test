d1f6041daab40b2ef387b560d9555938a863ddf6
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-10-24 04:14:00 +0000

    Improvement of 03b/proc.py

--- trunk/03b/proc.py
+++ trunk/03b/proc.py
@@ -7,12 +7,12 @@ sys.path.append("/home/current/proj/PyMS/")
 from pyms.Experiment.IO import load_amdis_expr
 from pyms.IO.ANDI.Class import Xcalibur
 
-# path to AMDIS ELU data file
-amdis_file = "/home/current/proj/PyMS/data/pyms-data/121107B_10.ELU"
-
 # path to ANDI-MS data which was used to generate AMDIS ELU data.
 andi_file = "/home/current/proj/PyMS/data/pyms-data/121107B_10.CDF"
 andi_data = Xcalibur(andi_file)
+
+# path to AMDIS ELU data file
+amdis_file = "/home/current/proj/PyMS/data/pyms-data/121107B_10.ELU"
 
 # This full data is an optional argument. If available the full mass
 # spectra for the peaks will be used, and not the AMDIS generated
