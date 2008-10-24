ccbe697d4b19e94d7eacb3f878b26a5b3bd17071
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-10-24 04:27:21 +0000

    Added peak alignment examples under new names

---
+++ trunk/61a/proc.py
@@ -0,0 +1,53 @@
+"""proc.py
+"""
+
+import sys, os
+sys.path.append("/home/current/proj/PyMS/")
+
+from pyms.IO.ANDI.Class import ChemStation
+from pyms.Experiment.Class import Experiment
+from pyms.Peak.List.IO import read_chem_station_peaks
+from pyms.Experiment.IO import dump_expr
+
+# define path to data files
+base_path = "/home/current/proj/PyMS/data/pyms-data/"
+
+# define experiments to process
+expr_codes = [ "a0806_077", "a0806_078", "a0806_079" ]
+
+# loop over all experiments
+for expr_code in expr_codes:
+
+    # define the names of the peak file and the corresponding ANDI-MS file
+    peak_file = os.path.join(base_path, expr_code + ".txt.a")
+    andi_file = os.path.join(base_path, expr_code + ".CDF")
+
+    # load ANDI-MS data and chemstation peak list
+    andi_data = ChemStation(andi_file)
+    peaks = read_chem_station_peaks(peak_file)
+
+    # null ion chromatograms for m/z=73 and m/z=147. These masses
+    # originate from the derivatiziing agent
+    andi_data.null_mass(73)
+    andi_data.null_mass(147)
+
+    # loop over peaks and add mass spectrum
+    for peak in peaks:
+        peak.set_mass_spectrum(andi_data)
+        peak.crop_mass_spectrum(50,540)
+
+    # create an experiment object
+    expr = Experiment(expr_code, peaks)
+
+    # set the reference peak, remove blank peaks, create norm area
+    # purge negative peaks, and select peaks from the range 6.5 min
+    # to 21 min
+    expr.set_ref_peak("si")
+    expr.remove_blank_peaks("blank")
+    expr.raw2norm_area()
+    expr.purge_peaks()
+    expr.sele_rt_range(["6.5m", "21m"])
+
+    # dump experiment to a file
+    dump_expr(expr, "output/" + expr_code + ".expr")
+
---
+++ trunk/62/proc.py
@@ -0,0 +1,28 @@
+"""proc.py
+"""
+
+import sys
+sys.path.append("/home/current/proj/PyMS/")
+
+from pyms.Experiment.IO import read_expr_list
+from pyms.Peak.List.DPA.Function import exprl2alignment
+from pyms.Peak.List.DPA.Class import PairwiseAlignment
+from pyms.Peak.List.DPA.Function import align_with_tree
+
+# define the input experiments list
+input1 = "input1"
+
+# within replicates alignment parameters
+Dw = 2.5  # rt modulation [s]
+Gw = 0.30 # gap penalty
+
+# do the alignment
+print 'Aligning input 1'
+E1 = read_expr_list(input1)
+F1 = exprl2alignment(E1)
+T1 = PairwiseAlignment(F1, Dw, Gw)
+A1 = align_with_tree(T1, min_peaks=2)
+
+# save the alignment matrices
+A1.write_csv('output/rt1.csv', 'output/area1.csv')
+
