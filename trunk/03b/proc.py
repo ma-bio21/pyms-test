7058425b4a9e277951657f84851e3dc16ed986b0
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-10-24 04:11:41 +0000

    Refined 03b/proc.py

--- trunk/03b/proc.py
+++ trunk/03b/proc.py
@@ -10,15 +10,17 @@ from pyms.IO.ANDI.Class import Xcalibur
 # path to AMDIS ELU data file
 amdis_file = "/home/current/proj/PyMS/data/pyms-data/121107B_10.ELU"
 
-# path to associated ANDI-MS data for AMDIS ELU data
-# This parameter is optional and will use the full mass spectra for
-# the peak and not the AMDIS generated mass spectra from the ELU file
+# path to ANDI-MS data which was used to generate AMDIS ELU data.
 andi_file = "/home/current/proj/PyMS/data/pyms-data/121107B_10.CDF"
 andi_data = Xcalibur(andi_file)
 
+# This full data is an optional argument. If available the full mass
+# spectra for the peaks will be used, and not the AMDIS generated
+# mass spectra from the ELU file
 expr = load_amdis_expr(amdis_file,andi_data)
 peaks = expr.peaks
 
 # the variable 'peaks' is a Python list
 type(peaks)
 print "The number of peaks is:", len(peaks)
+
