9566922b91fbb84ed88ae789d0439b48cb5cea54
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-10-24 04:45:15 +0000

    Added example 68/

---
+++ trunk/68/proc.py
@@ -0,0 +1,20 @@
+"""proc.py
+"""
+
+import sys
+sys.path.append("/home/current/proj/PyMS/")
+
+from pyms.Experiment.IO import load_amdis_expr,load_xcalibur_expr
+from pyms.Peak.List.Metric import metric
+
+# path to ANDI-MS data file and Xcalibur and AMDIS peak files
+andi_data = "/home/current/proj/PyMS/data/pyms-data//121107B_10.CDF"
+xcalibur_peaks = "/home/current/proj/PyMS/data/pyms-data/121107B_10_xcalibur_peaks.txt"
+amdis_peaks = "/home/current/proj/PyMS/data/pyms-data//121107B_10.ELU"
+
+amdis_expr = load_amdis_expr(amdis_peaks)
+xcalibur_expr = load_xcalibur_expr(xcalibur_peaks,andi_data)
+
+# compare peak lists, verbose
+metric_result = metric(amdis_expr.peaks, xcalibur_expr.peaks,verbose=True)
+
