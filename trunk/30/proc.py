4617fdcc61c0038e834c530caac10c914ad924da
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-12-15 07:20:25 +0000

    Developed 30/proc.py

--- trunk/30/proc.py
+++ trunk/30/proc.py
@@ -8,6 +8,8 @@ from pyms.IO.ANDI.Class import ChemStation
 from pyms.Noise.SavitzkyGolay import savitzky_golay
 from pyms.Baseline.TopHat import tophat
 from pyms.Noise.Function import analyze_noise 
+from pyms.Peak.Detection.MinMax import minmax
+from pyms.Peak.List.IO import write_peaks
 
 # read the raw data
 andi_file = "/x/proj.archive/proj/PyMS/data/a0806_140.CDF"
@@ -18,4 +20,9 @@ tic = data.get_tic()
 tic1 = savitzky_golay(tic)
 tic_bc = tophat(tic1, struct="1.5m")
 noise = analyze_noise(tic_bc)
+peaks = minmax(tic_bc, 1.5*noise, window="1.7s")
+
+# write TIC and peaks
+tic_bc.write("output/tic.dat")
+write_peaks(peaks, "output/peaks.dat")
 
