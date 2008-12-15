5deed1b1f82381042d79a5c4d7537dd8453cc930
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-12-15 23:48:29 +0000

    Milica's latest changes

--- trunk/09/proc.py
+++ trunk/09/proc.py
@@ -6,7 +6,7 @@ import numpy
 import sys
 sys.path.append("/x/proj.archive/proj/PyMS/")
 
-from pyms.Isotope.MFRA.Function import correction_matrix, mass_dist_vector, c_mass_isotope_distr
+from pyms.Isotope.MFRA.Function import overall_correction_matrix, correction_matrix, c_mass_isotope_distr, mass_dist_vector
 from pyms.Isotope.MFRA.Constants import nsi
 
 # -- input data ---
@@ -27,38 +27,29 @@ atoms = {
 f_unlablelled = 0.01
 # -- end of input data --
 
-# calculate n, this could work depending on the content of the  input file
+# calculate n
 n = len(mdv)-1
 
-atom_symbols = atoms.keys() 
-
-c_corr = numpy.eye(n+1) 
-for a in atom_symbols:
-    print '\n Calculating %s correction matrix' % ( a )
-    m_corr = correction_matrix(n, atoms[a], nsi[a])
-    c_corr = numpy.dot(c_corr,m_corr)
-
-print('\nOverall correction matrix:')
+# the overall correction matrix
+c_corr = overall_correction_matrix(n, mdv, atoms, nsi)
+print('\n Overall correction matrix:')
 print(c_corr)
 
 # the exclusive mass isotope distribution of the carbon skeleton, mdv_alpha_star
 mdv_alpha_star = c_mass_isotope_distr(mdv, c_corr)
-print('\nNormalised mdv alpha star:')
+print('\n Normalised mdv alpha star:')
 print(mdv_alpha_star)
 
 # correction for unlabelled biomass
 mdv_unlabelled = mass_dist_vector(n, n, nsi['c'])
 mdv_aa = (mdv_alpha_star - f_unlablelled * mdv_unlabelled)/(1 - f_unlablelled)
-print('\nmdv_aa:')
+print('\n mdv_aa:')
 print(mdv_aa)
 
-# For [U-13C]glucose experiments, the fractional labelling (FL) of
-# the different amino fragments should be equal to the labelling content of
-# the input substrate (i.e. when 20% [U-13C]glucose is used, the FL should
-# be 0.2 for all fragments).
+# For [U-13C]glucose experiments, the fractional labelling (FL) of the different
+# fragments should be equal to the labelling content of the input substrate 
+# (i.e. when 20% [U-13C]glucose is used, the FL should be 0.2 for all fragments)
 mdv_aa = numpy.matrix(mdv_aa)
 fl = (range(0,(n+1)) * mdv_aa) / (n * numpy.sum(mdv_aa))
-print('\nFractional labelling FL:')
+print('\n Fractional labelling FL:')
 print(fl)
-
-
