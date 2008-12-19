dfde051f244a647b91264cc22ad314b9f4450f9b
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2008-12-19 01:52:35 +0000

    Amended 41/proc.py

--- trunk/41/proc.py
+++ trunk/41/proc.py
@@ -4,6 +4,7 @@
 import sys
 sys.path.append("/x/proj.archive/proj/PyMS/")
 
+from pyms.Utils.IO import save_data
 from pyms.Simulator.Class import GCMS_simulator
 
 s = GCMS_simulator(rt=['5.1m','21m',0.343], mz=[51,550,1])
@@ -16,3 +17,4 @@ print rt
 print mz
 print im
 
+save_data("output/im.dat", im)
