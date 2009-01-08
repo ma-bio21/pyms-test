d7b812ac5e2a3919dab5c4a5a7736c321e0cbb7f
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-01-08 06:31:51 +0000

    Style changes after initial import

--- trunk/65/proc.py
+++ trunk/65/proc.py
@@ -14,16 +14,16 @@ andi_file1 = "/x/proj.archive/proj/PyMS/data/a0806_077.CDF"
 andi_file2 = "/x/proj.archive/proj/PyMS/data/a0806_140.CDF"
 
 # get the intensity matrix and save to a file
-andi_data1 = ChemStation(andi_file1)
-andi_data2 = ChemStation(andi_file2)
+data1 = ChemStation(andi_file1)
+data2 = ChemStation(andi_file2)
 
 # print the name of the ANDI-MS file
-print "ANDI-MS data 1 filename:", andi_data1.get_filename()
-print "ANDI-MS data 2 filename:", andi_data2.get_filename()
+print "ANDI-MS data 1 filename:", data1.get_filename()
+print "ANDI-MS data 2 filename:", data2.get_filename()
 
 # get the entire intensity matrix
-im1 = andi_data1.get_intensity_matrix()
-im2 = andi_data2.get_intensity_matrix()
+im1 = data1.get_intensity_matrix()
+im2 = data2.get_intensity_matrix()
 
 print "Dimensions of the intensity matrix 1 are:",len(im1),"x",len(im1[0])
 print "Dimensions of the intensity matrix 2 are:",len(im2),"x",len(im2[0])
@@ -33,5 +33,6 @@ Gw =0.30
 E1 = [im1,im2]
 F1 = exprl2alignment(E1)
 T1 = PairwiseAlignment(F1, Gw)
-#A1 = align_with_tree(T1)
-print "Job done"
+
+print "All done"
+
