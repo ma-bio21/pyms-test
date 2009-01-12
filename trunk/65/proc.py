4ab7558b99612b3aa388be0e334e4aa2dc76bbb0
tadaniajoe <tadaniajoe@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-01-12 06:18:03 +0000

    Rubbish code cleaned up for example 65

--- trunk/65/proc.py
+++ trunk/65/proc.py
@@ -2,37 +2,21 @@
 """
 
 import sys
-sys.path.append("/x/proj.archive/proj/PyMS/")
+sys.path.append("/home/qiaow/Desktop/pyms-read-only")
 
 from pyms.IO.ANDI.Class import ChemStation
-from pyms.Alignment.Function import exprl2alignment
-from pyms.Alignment.Function import align_with_tree
-from pyms.Alignment.Class import PairwiseAlignment
+from pyms.Alignment.Function import fma
 
 # path to ANDI-MS data file
-andi_file1 = "/x/proj.archive/proj/PyMS/data/a0806_077.CDF"
-andi_file2 = "/x/proj.archive/proj/PyMS/data/a0806_140.CDF"
+andi_file1 = "/home/qiaow/PyMS/pyms-data/a0806_077.CDF"
+andi_file2 = "/home/qiaow/PyMS/pyms-data/a0806_140.CDF"
 
-# get the intensity matrix and save to a file
-data1 = ChemStation(andi_file1)
-data2 = ChemStation(andi_file2)
-
-# print the name of the ANDI-MS file
-print "ANDI-MS data 1 filename:", data1.get_filename()
-print "ANDI-MS data 2 filename:", data2.get_filename()
-
-# get the entire intensity matrix
-im1 = data1.get_intensity_matrix()
-im2 = data2.get_intensity_matrix()
-
-print "Dimensions of the intensity matrix 1 are:",len(im1),"x",len(im1[0])
-print "Dimensions of the intensity matrix 2 are:",len(im2),"x",len(im2[0])
+# import the GC-MS data
+andi_data1 = ChemStation(andi_file1)
+andi_data2 = ChemStation(andi_file2)
 
 Gw =0.30
 
-E1 = [im1,im2]
-F1 = exprl2alignment(E1)
-T1 = PairwiseAlignment(F1, Gw)
+fma(andi_data1, andi_data2, Gw)
 
 print "All done"
-
