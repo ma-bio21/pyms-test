fb0bc3f7a0bf4c04876916d7b99019857f580602
tadaniajoe <tadaniajoe@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-01-12 06:26:22 +0000

    Rubbish code cleaned up for example 65

--- trunk/65/proc.py
+++ trunk/65/proc.py
@@ -2,14 +2,14 @@
 """
 
 import sys
-sys.path.append("/home/qiaow/Desktop/pyms-read-only")
+sys.path.append("/x/proj.archive/proj/PyMS")
 
 from pyms.IO.ANDI.Class import ChemStation
 from pyms.Alignment.Function import fma
 
 # path to ANDI-MS data file
-andi_file1 = "/home/qiaow/PyMS/pyms-data/a0806_077.CDF"
-andi_file2 = "/home/qiaow/PyMS/pyms-data/a0806_140.CDF"
+andi_file1 = "/x/proj.archive/proj/PyMS/data/a0806_077.CDF"
+andi_file2 = "/x/proj.archive/proj/PyMS/data/a0806_140.CDF"
 
 # import the GC-MS data
 andi_data1 = ChemStation(andi_file1)
