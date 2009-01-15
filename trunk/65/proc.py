f85c5ea8fce4329d5173a1850734e4e3211e0561
tadaniajoe <tadaniajoe@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-01-15 04:02:11 +0000

    updated example 65

--- trunk/65/proc.py
+++ trunk/65/proc.py
@@ -4,7 +4,11 @@
 import sys
 sys.path.append("/x/proj.archive/proj/PyMS")
 
+import pyms.Alignment.Function
+import pyms.Alignment.Class
+
 from pyms.IO.ANDI.Class import ChemStation
+from pyms.Alignment.Class import Alignment
 from pyms.Alignment.Function import fma
 
 # path to ANDI-MS data file
@@ -14,9 +18,10 @@ andi_file2 = "/x/proj.archive/proj/PyMS/data/a0806_140.CDF"
 # import the GC-MS data
 data1 = ChemStation(andi_file1)
 data2 = ChemStation(andi_file2)
-
+a1 = Alignment(data1)
+a2 = Alignment(data2)
 Gw =0.30
 
-fma(data1, data2, Gw)
+fma(a1, a2, Gw)
 
 print "All done"
