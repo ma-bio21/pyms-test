3a63e1ba071fff7fcec4923a015fa91c6d4ae4f4
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-01-19 03:21:02 +0000

    Added example for reading mass spectral libraries in JCAMP format

---
+++ trunk/71/proc.py
@@ -0,0 +1,16 @@
+"""proc.py
+"""
+
+import sys
+sys.path.append("/x/proj.archive/proj/PyMS/")
+
+from pyms.libMS.JCAMP.IO import load_jcamp
+
+ms_lib_file = "/x/proj.archive/proj/PyMS/data/mslib.jcamp"
+
+records = load_jcamp(ms_lib_file)
+
+for r in records:
+    print r.name
+    print r.mass_spectrum
+
