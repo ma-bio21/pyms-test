1801dd861b926a83e3bc86a2aa442a369ecc325c
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-01-20 23:09:01 +0000

    Added reading of the test data file, and preparation of the MS library for search.

--- trunk/71/proc.py
+++ trunk/71/proc.py
@@ -4,13 +4,21 @@
 import sys
 sys.path.append("/x/proj.archive/proj/PyMS/")
 
-from pyms.libMS.JCAMP.IO import load_jcamp
+from pyms.libMS.JCAMP.Class import MSLib
+from pyms.IO.ANDI.Class import ChemStation
 
-ms_lib_file = "/x/proj.archive/proj/PyMS/data/mslib.jcamp.broken"
+ms_lib_file = "/x/proj.archive/proj/PyMS/data/mslib.jcamp"
+andi_file = "/x/proj.archive/proj/PyMS/data/a0806_140.CDF"
 
-records = load_jcamp(ms_lib_file)
+ms_lib = MSLib(ms_lib_file)
 
-for r in records:
+data = ChemStation(andi_file)
+mass_list = data.get_mass_list()
+
+ms_lib.set_for_search(mass_list)
+
+for r in ms_lib.records:
     print r.name
+    print r.mass_record
     print r.mass_spectrum
 
