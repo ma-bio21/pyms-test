91222e6ef9cb80d15cfe9e06caaaa88d536b6e1c
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-02-03 21:19:34 +0000

    Limited this test to library reading

--- trunk/71/proc.py
+++ trunk/71/proc.py
@@ -7,21 +7,10 @@ sys.path.append("/x/proj.archive/proj/PyMS/")
 from pyms.libMS.JCAMP.Class import MSLib
 from pyms.IO.ANDI.Class import ChemStation
 
-ms_lib_file = "mslib.jcamp"
+ms_lib_file = "/x/proj.archive/proj/PyMS/data/mslib.jcamp"
 andi_file = "/x/proj.archive/proj/PyMS/data/a0806_140.CDF"
-
 
 print "Loading start at:", time.localtime()
 ms_lib = MSLib(ms_lib_file)
 print "Loading end at:", time.localtime()
 
-data = ChemStation(andi_file)
-mass_list = data.get_mass_list()
-im = data.get_intensity_matrix()
-im = im[0]
-
-print "Processing start at: ", time.localtime()
-name, max = ms_lib.match(mass_list, im)
-print name
-print max
-print "Processing end at: ", time.localtime()
