2eb93504b65b4fcd0a6aded91371cb6a6acbad0b
tadaniajoe <tadaniajoe@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-02-03 03:25:14 +0000

    Unknown compound matching example

--- trunk/71/proc.py
+++ trunk/71/proc.py
@@ -1,24 +1,27 @@
 """proc.py
 """
-
+import time
 import sys
 sys.path.append("/x/proj.archive/proj/PyMS/")
 
 from pyms.libMS.JCAMP.Class import MSLib
 from pyms.IO.ANDI.Class import ChemStation
 
-ms_lib_file = "/x/proj.archive/proj/PyMS/data/mslib.jcamp"
+ms_lib_file = "mslib.jcamp"
 andi_file = "/x/proj.archive/proj/PyMS/data/a0806_140.CDF"
 
+
+print "Loading start at:", time.localtime()
 ms_lib = MSLib(ms_lib_file)
+print "Loading end at:", time.localtime()
 
 data = ChemStation(andi_file)
 mass_list = data.get_mass_list()
+im = data.get_intensity_matrix()
+im = im[0]
 
-ms_lib.set_for_search(mass_list)
-
-for r in ms_lib.records:
-    print r.name
-    print r.mass_record
-    print r.mass_spectrum
-
+print "Processing start at: ", time.localtime()
+name, max = ms_lib.match(mass_list, im)
+print name
+print max
+print "Processing end at: ", time.localtime()
