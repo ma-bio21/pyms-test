8888422e95af4a4c63ba9e863b314c67e7722793
tadaniajoe <tadaniajoe@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-02-19 01:59:46 +0000

    matching a single scan point

--- trunk/72/proc.py
+++ trunk/72/proc.py
@@ -3,27 +3,28 @@
 import sys
 import time
 
-sys.path.append("/x/proj.archive/proj/PyMS/")
+sys.path.append("/home/qiaow/Desktop/pyms-read-only")
 
 from pyms.libMS.JCAMP.Class import MSLib
 from pyms.libMS.JCAMP.Class import MatchedObj
+from pyms.libMS.JCAMP.Class import MassSpectrum
 from pyms.libMS.Function import ms_lib_match
 from pyms.IO.ANDI.Class import ChemStation
 
-ms_lib_file = "/x/proj.archive/proj/PyMS/data/mslib.jcamp"
-andi_file = "/x/proj.archive/proj/PyMS/data/a0806_140.CDF"
+ms_lib_file = "/home/qiaow/PyMS/pyms-data/NIST08.JCAMP"
+andi_file = "/home/qiaow/PyMS/pyms-data/a0806_140.CDF"
+
+# load the GC-MS data
+data = ChemStation(andi_file)
 
 print "Loading start at:", time.localtime()
 ms_lib = MSLib(ms_lib_file)
 print "Loading end at:", time.localtime()
 
-# load the GC-MS data
-data = ChemStation(andi_file)
-
 # search the library for the scan 536
 
 # get the scan 536
-ms = data.get_ms_at_index(536)
+ms = MassSpectrum(data, 536)
 
 # match against the library
 result = ms_lib_match(ms_lib, ms)
@@ -33,4 +34,3 @@ matched_obj = MatchedObj(result)
 print "Best hit is:", matched_obj.compound
 print "Best hit score:", matched_obj.score
 print "Best hit mass spectrum:", matched_obj.mass_spec
-
