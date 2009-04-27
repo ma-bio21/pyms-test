85f05d98cad580ce14c6edb5370b8305cb0ae824
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-04-27 06:35:40 +0000

    Examples changed to follow change of JCAMP_reader() and ANDI_reader() from a function to a class

--- 01a/proc.py
+++ 01a/proc.py
@@ -4,32 +4,29 @@
 import sys
 sys.path.append("/x/PyMS")
 
-from pyms.GCMS.IO.JCAMP.Class import JCAMP_reader
+from pyms.GCMS.IO.JCAMP.Function import JCAMP_reader
 
 # read the raw data
 jcamp_file = "/x/PyMS/data/gc01_0812_066.jdx"
 data = JCAMP_reader(jcamp_file)
 
-# get the raw GCMS data
-gcms_data = data.get_gcms_data()
-
 # raw data operations
-print "minimum mass found in all data: ", gcms_data.get_min_mass()
-print "maximum mass found in all data: ", gcms_data.get_max_mass()
+print "minimum mass found in all data: ", data.get_min_mass()
+print "maximum mass found in all data: ", data.get_max_mass()
 
 # time
-time = gcms_data.get_time_list()
+time = data.get_time_list()
 print "number of retention times: ", len(time)
 print "retention time of 1st scan: ", time[0], "sec"
-print "index of 400sec in time_list: ", gcms_data.get_index_at_time(400.0)
+print "index of 400sec in time_list: ", data.get_index_at_time(400.0)
 
 # TIC
-tic = gcms_data.get_tic()
+tic = data.get_tic()
 print "number of scans in TIC: ", len(tic)
 print "start time of TIC: ", tic.get_time_at_index(0), "sec"
 
 # raw scans
-scans = gcms_data.get_scan_list()
+scans = data.get_scan_list()
 
 print "number of masses in 1st scan: ", len(scans[0])
 print "1st mass value for 1st scan: ", scans[0].get_mass_list()[0]
