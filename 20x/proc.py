8684ab2833cf90113ae29644028a16f0987e0e7c
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-05-11 22:42:12 +0000

    Added example 20x/ -- diff() function on GCMS_data objects

---
+++ 20x/proc.py
@@ -0,0 +1,19 @@
+"""proc.py
+"""
+
+import sys
+sys.path.append("/x/PyMS")
+
+from pyms.GCMS.IO.JCAMP.Function import JCAMP_reader
+from pyms.GCMS.IO.ANDI.Function import ANDI_reader
+from pyms.GCMS.Function import diff
+
+# read the raw data
+andi_file = "/x/PyMS/data/gc01_0812_066.cdf"
+jcamp_file = "/x/PyMS/data/gc01_0812_066.jdx"
+
+data1 = ANDI_reader(andi_file)
+data2 = JCAMP_reader(jcamp_file)
+
+diff(data1,data2)
+
