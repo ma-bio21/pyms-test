97b61851f8e9efea850b5938306b1a1f6b6c6530
andrew.isaac.au <andrew.isaac.au@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-05-18 05:53:33 +0000

    1st implimentation of Biler Biemann

--- trunk/x01/proc.py
+++ trunk/x01/proc.py
@@ -4,17 +4,18 @@
 import sys
 sys.path.append("/x/PyMS")
 
-from pyms.GCMS.IO.JCAMP.Function import JCAMP_reader
+from pyms.GCMS.IO.ANDI.Function import ANDI_reader
 from pyms.GCMS.Function import build_intensity_matrix, build_intensity_matrix_i
 from pyms.GCMS.IO.Function import export_csv, export_leco_csv
 from pyms.Utils.IO import save_data
 
 from pyms.Deconvolution.BillerBiemann.Functions import get_maxima_indices, \
-    get_maxima_list
+    get_maxima_list, get_maxima_matrix, BillerBiemann
 
 # read the raw data as a GCMS_data object
-jcamp_file = "/x/PyMS/data/gc01_0812_066.jdx"
-data = JCAMP_reader(jcamp_file)
+#jcamp_file = "/x/PyMS/data/gc01_0812_066.jdx"
+andi_file = "/x/PyMS/data/gc01_0812_073.cdf"
+data = ANDI_reader(andi_file)
 
 # IntensityMatrix
 # must build intensity matrix before accessing any intensity matrix methods.
@@ -73,3 +74,8 @@ for index in range(len(ic)):
     rt = ic.get_time_at_index(index)
     out.append([rt, count[index]])
 save_data("output/counts.csv", out)
+
+# biller biemann
+print "Biller Biemann reconstructed TIC"
+bbtic = BillerBiemann(im)
+bbtic.write("output/bbtic.csv")
