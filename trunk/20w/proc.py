3ca0e1c2c4c6fb88891fadbdf30b1da3862bc830
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-05-19 05:45:24 +0000

    Improved example 20w/

--- trunk/20w/proc.py
+++ trunk/20w/proc.py
@@ -8,11 +8,13 @@ from pyms.GCMS.IO.ANDI.Function import ANDI_reader
 
 # read the raw data
 andi_file = "/x/PyMS/data/gc01_0812_066.cdf"
-andi_file = "/x/PyMS/data/JP_MM_TOF.cdf"
-data1 = ANDI_reader(andi_file)
+data = ANDI_reader(andi_file)
 
 # print info
-data1.write("output/data")
-#data1.info(print_scan_n=True)
-data1.info()
+data.info()
+
+# write data to output file. This will create
+# two ascii data tables, data.I and data.mz
+# with intensities and m/z values
+data.write("output/data")
 
