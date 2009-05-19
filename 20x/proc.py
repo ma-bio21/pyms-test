8b52b06815b2f53642aa1052678b8402b1e1578f
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-05-19 02:31:27 +0000

    Added trimmed data

--- 20x/proc.py
+++ 20x/proc.py
@@ -15,5 +15,8 @@ jcamp_file = "/x/PyMS/data/gc01_0812_066.jdx"
 data1 = ANDI_reader(andi_file)
 data2 = JCAMP_reader(jcamp_file)
 
+# trim data2 between scans 1000 and 2000
+#data2.trim(begin=1000,end=2000)
+
 diff(data1,data2)
 
