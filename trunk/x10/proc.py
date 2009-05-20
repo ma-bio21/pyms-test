710c3d1ad02b4e598a3013c3aaae2965d2e2d395
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-05-20 03:25:14 +0000

    Added x10, the initial example for VPAC

---
+++ trunk/x10/proc.py
@@ -0,0 +1,28 @@
+"""proc.py
+"""
+
+import sys
+sys.path.append("/x/PyMS")
+
+from pyms.GCMS.IO.JCAMP.Function import JCAMP_reader
+from pyms.GCMS.Function import build_intensity_matrix_i
+from pyms.Noise.Window import window_smooth
+
+# read the raw data as a GCMS_data object
+jcamp_file = "/x/PyMS/data/gc01_0812_066.jdx"
+data = JCAMP_reader(jcamp_file)
+
+# build the intensity matrix
+im = build_intensity_matrix_i(data)
+
+# get the size of the intensity matrix
+n_scan, n_mz = im.get_size()
+print "Size of the intensity matrix is (n_scans, n_mz):", n_scan, n_mz
+
+# loop over all m/z values, fetch the corresponding IC, and perform
+# noise smoothing
+for ii in range(n_mz):
+    print ii+1,
+    ic = im.get_ic_at_index(ii)
+    ic_smooth = window_smooth(ic, window=7) 
+
