de0505b690e08219cdcacccf55b07bf32948bd1a
andrew.isaac.au <andrew.isaac.au@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-06-04 06:14:54 +0000

    deconvolution eg

--- x01/proc.py
+++ x01/proc.py
@@ -4,7 +4,6 @@
 import sys
 sys.path.append("/x/PyMS")
 
-#from pyms.GCMS.IO.ANDI.Function import ANDI_reader
 from pyms.GCMS.IO.JCAMP.Function import JCAMP_reader
 from pyms.GCMS.Function import build_intensity_matrix, build_intensity_matrix_i
 from pyms.GCMS.IO.Function import export_csv, export_leco_csv
@@ -12,13 +11,12 @@ from pyms.Utils.IO import save_data
 
 from pyms.Peak.Class import Peak
 from pyms.Deconvolution.BillerBiemann.Functions import get_maxima_indices, \
-    get_maxima_list, get_maxima_matrix, sum_maxima, BillerBiemann
+    get_maxima_list, get_maxima_matrix, sum_maxima, BillerBiemann, \
+    rel_threshold, num_ions_threshold
 
 # read the raw data as a GCMS_data object
 jcamp_file = "/x/PyMS/data/gc01_0812_066.jdx"
 data = JCAMP_reader(jcamp_file)
-#andi_file = "/x/PyMS/data/gc01_0812_073.cdf"
-#data = ANDI_reader(andi_file)
 
 # IntensityMatrix
 # must build intensity matrix before accessing any intensity matrix methods.
@@ -27,62 +25,34 @@ data = JCAMP_reader(jcamp_file)
 print "default intensity matrix, bin size = 1"
 im = build_intensity_matrix(data)
 
-#print "size of intensity matrix (#scans, #bins):", im.get_size()
-
-#masses = im.get_mass_list()
-
-
-## TIC and SIC
-
-## TIC from raw data
-#tic = data.get_tic()
-## save TIC to a file
-#tic.write("output/tic.dat",minutes=True)
-
-#index = im.get_index_of_mass(73)
-#ic = im.get_ic_at_index(index)
-#ic.write("output/ic_mass_73.dat",minutes=True)
-
-#peaks = get_maxima_list(ic)
-#save_data("output/peaks.csv", peaks)
-
-## Export the entire IntensityMatrix as LECO CSV. This is
-## useful for import into AnalyzerPro
-#print "exporting intensity matrix data to LECO CSV format..."
-#export_leco_csv("output/data_leco.csv", im)
-
-
-## get all maxima for all ions
-#print "finding all maxima"
-#peaks_matrix = []
-#for mass in masses:
-    #ic = im.get_ic_at_mass(mass)
-    #ia = ic.get_intensity_array()
-    #peaks_matrix.append(get_maxima_indices(ia))
-
-## count all maxima across all ions
-#print "creating histogram"
-#count = []
-#for index in range(len(ic)):
-    #num = 0
-    #for ii in range(len(peaks_matrix)):
-        #if index in peaks_matrix[ii]:
-            #num += 1
-    #count.append(num)
-
-## wrap it up an save
-#print "saving histogram"
-#out = []
-#for index in range(len(ic)):
-    #rt = ic.get_time_at_index(index)
-    #out.append([rt, count[index]])
-#save_data("output/counts.csv", out)
-
-## biller biemann
-#print "Biller Biemann reconstructed TIC"
-#bbtic = sum_maxima(im)
-#bbtic.write("output/bbtic.csv")
+# thresholds
+# filter width, maxima in 'points' is apex
+points = 9
+# minimum number of ions, n
+n = 3
+# greater than or equal to threshold, t
+t = 10000
+# percentage ratio of ion intensity to max ion intensity
+r = 2
 
 # get the list of Peak objects
-pl = BillerBiemann(im)
-print "Number of Peaks found:", len(pl)
+pl = BillerBiemann(im, points)
+#print "Number of Peaks found:", len(pl)
+
+# trim by relative intensity
+apl = rel_threshold(pl, r)
+
+# trim by threshold
+bpl = num_ions_threshold(apl, n, t)
+
+# output details of peaks
+print "\"rt\",\"TIC\",\"number_ions\",\"sorted_intensities...\""
+for p in bpl:
+    ms = p.get_mass_spectrum().mass_spec
+    l = []
+    for i in ms:
+        if i > 0:
+            l.append(i)
+    if len(l) > 0:
+        print p.get_rt(), sum(l), len (l), sorted(l)
+print
