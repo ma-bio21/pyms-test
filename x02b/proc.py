16ff4bbf2796ad91118effab51003bf7447d92dc
andrew.isaac.au <andrew.isaac.au@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-07-22 01:51:59 +0000

    mass null and crop for Expts

--- x02b/proc.py
+++ x02b/proc.py
@@ -13,12 +13,26 @@ from pyms.Utils.IO import save_data
 # test to create, store and re-load experiment
 peak_list = load_peaks("../x02a/output/MixMA-5C-1.peaks")
 
+print "Original peak list length:", len(peak_list)
+
+# ignore TMS ions and use same mass range for all experiments
+for peak in peak_list:
+    peak.crop_mass(50,540)
+    peak.null_mass(73)
+    peak.null_mass(147)
+
+# create an experiment
 expr = Experiment("MixMA-5C-1", peak_list)
+
+# use same time range for all experiments
+expr.sele_rt_range(["6.5m", "21m"])
 
 store_expr("output/MixMA-5C-1.expt", expr)
 
 # check stored
 expr2 = load_expr("output/MixMA-5C-1.expt")
 
-print "saved:", expr.get_name(), "peak list len:", len(expr.get_peak_list())
-print "loaded:", expr2.get_name(), "peak list len:", len(expr2.get_peak_list())
+print "saved:", expr.get_expr_code(), "peak list len:", \
+    len(expr.get_peak_list())
+print "loaded:", expr2.get_expr_code(), "peak list len:", \
+    len(expr2.get_peak_list())
