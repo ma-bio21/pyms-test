003a0f48f42ccaa59993152dec86002a329d5716
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-07-24 00:09:21 +0000

    Preparations for 53/

--- trunk/x01/proc.py
+++ trunk/x01/proc.py
@@ -6,9 +6,7 @@ sys.path.append("/x/PyMS")
 
 from pyms.GCMS.IO.JCAMP.Function import JCAMP_reader
 from pyms.GCMS.Function import build_intensity_matrix, build_intensity_matrix_i
-from pyms.GCMS.IO.Function import export_csv, export_leco_csv
 from pyms.Utils.IO import save_data
-
 from pyms.Peak.Class import Peak
 from pyms.Deconvolution.BillerBiemann.Functions import get_maxima_indices, \
     get_maxima_list, get_maxima_matrix, sum_maxima, BillerBiemann, \
