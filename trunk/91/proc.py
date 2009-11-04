2ce60d7ff8009385d9198ad79b449dcaa7ef5dd2
milica.ng14 <milica.ng14@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-11-04 01:43:47 +0000

    path names changed

--- trunk/91/proc.py
+++ trunk/91/proc.py
@@ -7,14 +7,14 @@ sys.path.append("/x/PyMS/")
 from pyms.Flux.MassExtraction.Function import extract_mid
 
 # -- input data ---
-file_base = '/home/projects/PyMS/data-0903/a0903_'
-file_numbers = (297, 298, 299, 301, 302, 303, 304, 306, 307, 308, 253, 255, 256, 257, 258, 260, 261, 262, 263, 265, 266, 267, 268, 270, 271, 272, 273)
+file_base = '/x/PyMS/data-0903/a0903_'
+file_numbers = (297, 298, 299)
 compound = 'Alanine'
 ion = 190
-rt = 411 # equals 6.85mins
+rt = 411 # seconds
 
 # -- parameters ---
-mid_size = 6 # mid_size is the total number of masses (M, M+1, ..., M+n), it equals n+1
+mid_size = 6 # mid_size (n+1) is the number of masses (M, M+1, ..., M+n)
 win_size = 4 # seconds
 
 # extract mass isotopomer distribution vectors
