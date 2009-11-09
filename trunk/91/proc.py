13a117a078b2db265980107a453eeddd266bb594
milica.ng14 <milica.ng14@7eee158e-254d-0410-9f01-bbecc1f07e93>
2009-11-09 02:15:44 +0000

    added output file

--- trunk/91/proc.py
+++ trunk/91/proc.py
@@ -10,18 +10,24 @@ from pyms.Flux.Class import MIDS
 from pyms.Flux.MassExtraction.Function import extract_mid
 
 # -- input data ---
+
 # data_file_root = '/x/PyMS/data-0903/a0903_'
 data_file_root = '/home/projects/PyMS/data-0903/a0903_'
 data_file_nums = (297, 298, 299)
-# out_file = '/x/PyMS/data-0903/a0903_'
-out_file = '/home/projects/PyMS/data-0903/output/output.csv' # todo: generalise
+# data_file_nums = (297, 298, 299, 301, 302, 303, 304, 306, 307, 308, 253, 255, 256, 257, 258, 260, 261, 262, 263, 265, 266, 267, 268, 270, 271, 272, 273)
+
+# out_file = '/x/PyMS/pyms-test/91/output/out.csv'
+out_file = '/home/projects/PyMS/pyms-test/91/output/out.csv'
 
 # set compound name, retention time, diagnostic ions and MID size
 mids = MIDS('alanine', 411, [190], 6)
 
+# -- end input data ---
+
 # -- parameters ---
 win_size = 4 # seconds
 noise = 4000 #todo: estimate from data files
+# -- end parameters ---
 
 # extract mass isotopomer distribution vectors
 mids = extract_mid(data_file_root, data_file_nums, mids, win_size, noise)
