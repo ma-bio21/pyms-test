64197e5dd4924b36fb2f414597a97407e95353d4
sean.ocall <sean.ocall@7eee158e-254d-0410-9f01-bbecc1f07e93>
2010-03-18 23:29:47 +0000

    Adjusted test scripts 70b and 71 to reflect changes in Display module

--- 71/proc.py
+++ 71/proc.py
@@ -51,17 +51,6 @@ ics = [ic, ic1, ic2, ic3]
 peak_list = load_peaks("output/peaks.bin") 
 
 
-# Display some information about the peaks
-# The peak list has only been formed over the 
-# time interval 500s to 2000s, so peaks outside
-# this interval are not listed or displayed
-for peak in peak_list:
-	UID = peak.get_UID()	
-	rt = peak.get_rt()
-	height = sum(peak.get_mass_spectrum().mass_spec)
-	print UID + ", %.2f, %.2f" % (rt, height)
-	
-
 
 # Create a new display object, this time plot four ICs 
 # and the TIC, as well as the peak list
@@ -70,4 +59,4 @@ display = Display()
 display.plot_tic(tic, 'TIC')
 display.plot_ics(ics, ['191','73','57','55'])
 display.plot_peaks(peak_list, 'Peaks')
-display.do_plotting('TIC, and PyMS Detected Peaks')
+display.do_plotting('TIC, ICs, and PyMS Detected Peaks')
