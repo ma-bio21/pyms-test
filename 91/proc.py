3661822b501c068f6946ba01573ff79243d67d71
milica.ng14 <milica.ng14@7eee158e-254d-0410-9f01-bbecc1f07e93>
2010-03-31 11:43:16 +0000

    minor changes

--- 91/proc.py
+++ 91/proc.py
@@ -3,8 +3,7 @@
 
 import sys
 
-# sys.path.append("/x/PyMS/")
-sys.path.append("/home/projects/PyMS")
+sys.path.append("/x/PyMS/")
 
 from pyms.GCMS.IO.ANDI.Function import ANDI_reader
 from pyms.GCMS.Function import build_intensity_matrix_i
@@ -12,31 +11,18 @@ from pyms.Flux.Class import MIDS
 from pyms.Flux.MassExtraction.Function import extract_mid
 
 # -- input data ---
-
-# data_file_root = '/x/PyMS/data/'
-data_file_root = '/home/projects/PyMS/data/0905/'
-# data_file_names_file = '/x/PyMS/pyms-test/91/input/file_names.txt'
-data_file_names_file = '/home/projects/PyMS/pyms-test/91/input/file_names.txt'
-
-# input file
-# in_file = '/x/PyMS/pyms-test/91/input/in.csv'
-in_file = '/home/projects/PyMS/pyms-test/91/input/in.csv'
-
-# output file
-# out_file = '/x/PyMS/pyms-test/91/output/out.csv'
-out_file = '/home/projects/PyMS/pyms-test/91/output/out.csv'
-
+data_file_root = '/x/PyMS/data/'
+data_file_names_file = '/x/PyMS/pyms-test/91/input/file_names.txt'
+in_file = '/x/PyMS/pyms-test/91/input/in.csv'
+out_file = '/x/PyMS/pyms-test/91/output/out.csv'
 # -- end input data ---
 
-# -- parameters ---
+# -- input parameters ---
+win_size = 4 # seconds, peak width is 1 to 1.5 win size 
+noise = 4000 #relates to the threshold of signal after which peaks lose shape/width goes down
+# -- end input parameters ---
 
-win_size = 4 # seconds
-noise = 4000 #todo: estimate from data files
-
-# -- end parameters ---
-
-# read input file
-
+# read in_file
 fp = open(in_file, 'r')
 lines = fp.readlines()
 mids_list = []
@@ -58,8 +44,7 @@ for line in lines:
 
 fp.close()
 
-# read data file names
-
+# read data_file_names_file
 fp = open(data_file_names_file, 'r')
 lines = fp.readlines()
 data_file_names = []
@@ -67,8 +52,7 @@ data_file_names = []
 for line in lines:
     data_file_names.append(line.strip())
 
-# loop over file names
-
+# loop over file names and extract MIDs
 for file_name in data_file_names:
 
     # load raw data
@@ -78,12 +62,13 @@ for file_name in data_file_names:
 
     # create intensity matrix
     im = build_intensity_matrix_i(data)
-    print 'built matrix'
+    print ' -> Intensity matrix built'
+
     # process data file to extract MIDs
     for mids in mids_list:
         mids = extract_mid(file_name, im, mids, win_size, noise)
 
-# write extracted MIDs to output file
+# write extracted MIDs, including any warnings, to out_file
 print '\n',' -> Writing to file ', out_file
 for mids in mids_list:       
     mids.write(out_file)
