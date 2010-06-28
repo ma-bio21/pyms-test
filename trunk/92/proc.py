3dccc9c35d7271ccee871d17017ab17d0d666e02
milica.ng14 <milica.ng14@7eee158e-254d-0410-9f01-bbecc1f07e93>
2010-06-28 07:09:41 +0000

    automated MID correction

--- trunk/92/proc.py
+++ trunk/92/proc.py
@@ -4,48 +4,45 @@
 import sys
 sys.path.append("/x/PyMS/")
 
-from pyms.MIDs.MassCorrection.Function import fract_labelling, corr_unlabelled, \
-    overall_correction_matrix, correction_matrix, c_mass_isotope_distr, \
-    mass_dist_vector
+from pyms.MIDs.Function import parse_mid_tables_file, parse_ion_file, write_mid_tables
+from pyms.MIDs.MassCorrection.Function import correct_mdv, fract_labelling, \
+    corr_unlabelled, overall_correction_matrix, correction_matrix, \
+    c_mass_isotope_distr, mass_dist_vector
 
 # -- input data ---
-# measured mass distribution vector
-mdv = [737537, 179694, 88657, 178433]
+mid_tables_file = '/x/PyMS/pyms-test/91/output/out.csv'
+ion_file = 'input/ion_composition'
+out_file = 'output/corrected-out.csv'
+# -- end input data ---
 
-# number of each atom type in the fragment
-atoms = {
- 'c' : 8,
- 'o' : 2,
- 'n' : 1,
- 'h' : 26,
- 'si' : 2,
- 's' : 0
-}
+# -- input parameters ---
+f_unlabelled = 0.01 # correction factor for unlabelled biomass
+# -- end input parameters ---
 
-# correction for unlabelled biomass
-f_unlabelled = 0.01
-# -- end of input data --
+# parse input files
+mid_table_list = parse_mid_tables_file(mid_tables_file)
+mid_table_list = parse_ion_file(ion_file, mid_table_list)
 
-# calculate n, this can work depending on the content of the  input file
-n = len(mdv)-1
+# loop through MID tables and correct
+for mid_table in mid_table_list:
 
-# the overall correction matrix
-c_corr = overall_correction_matrix(n, mdv, atoms)
-print('\n Overall correction matrix:')
-print(c_corr)
+    table_values = mid_table.get_values()
+    atoms = mid_table.get_atoms()
 
-# the exclusive mass isotope distribution of the carbon skeleton, mdv_alpha_star
-mdv_alpha_star = c_mass_isotope_distr(mdv, c_corr)
-print('\n Normalised mdv alpha star:')
-print(mdv_alpha_star)
+    # correct mdv values 
+    for file_name, mdv in table_values.iteritems():
+        mdv = correct_mdv(mdv, atoms, f_unlabelled)
+        # fill the MID table with mdv values
+        mid_table.set_values(mdv, file_name)
 
-# correction for unlabelled biomass
-mdv_aa = corr_unlabelled(n, mdv_alpha_star, f_unlabelled)
-print('\n mdv_aa:')
-print(mdv_aa)
+# write corrected MID tables to out_file
+print '\n',' -> Writing to file ', out_file
+write_mid_tables(mid_table_list, out_file)
 
-# For [U-13C]glucose experiments, the fractional labelling (FL) of the different
-# fragments should be equal to the labelling content of the input substrate 
-# (i.e. when 20% [U-13C]glucose is used, the FL should be 0.2 for all fragments).
-fl = fract_labelling(n, mdv_aa)
-print('\n Fractional labelling FL: %s' % ( fl ))
+
+
+
+
+
+
+
