850446388fb7ab603bf13335e46953da9543c941
sean.ocall <sean.ocall@7eee158e-254d-0410-9f01-bbecc1f07e93>
2010-08-12 05:13:36 +0000

    Added scripts to describe new Intensity Matrix Filtering functions

--- 42b/proc.py
+++ 42b/proc.py
@@ -6,7 +6,7 @@ sys.path.append("/x/PyMS/")
 
 from pyms.GCMS.IO.ANDI.Function import ANDI_reader
 from pyms.GCMS.Function import build_intensity_matrix
-from pyms.Noise.Window import window_smooth_im
+from pyms.Baseline.TopHat import tophat_im
 
 # read the raw data
 andi_file = "/x/PyMS/data/gc01_0812_066.cdf"
@@ -15,16 +15,16 @@ data = ANDI_reader(andi_file)
 # build an intensity matrix object from the data
 im = build_intensity_matrix(data)
 
-# Use window averaging to smooth all IC's in the IM
+# Use TopHat baseline correction on all IC's in the IM
 print "Smoothing ..."
-im_smooth = window_smooth_im(im, window=5, median=False)
+im_base_corr = tophat_im(im, struct="1.5m")
 print "Done"
 
 # find the IC for derivatisation product ion before smoothing
 ic = im.get_ic_at_index(73)
 
 # find the IC for derivatisation product ion after smoothing
-ic_smooth = im_smooth.get_ic_at_index(73)
+ic_base_corr = im_base_corr.get_ic_at_index(73)
 
 ic.write("output/ic.dat",minutes=True)
-ic_smooth.write("output/ic_smooth.dat",minutes=True)
+ic_base_corr.write("output/ic_smooth.dat",minutes=True)
