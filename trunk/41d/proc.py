3bad4fc34bfb76538a4f0a43bca3d15c99bd5f57
sean.ocall <sean.ocall@7eee158e-254d-0410-9f01-bbecc1f07e93>
2010-08-12 05:13:36 +0000

    Added scripts to describe new Intensity Matrix Filtering functions

--- trunk/41d/proc.py
+++ trunk/41d/proc.py
@@ -6,7 +6,7 @@ sys.path.append("/x/PyMS/")
 
 from pyms.GCMS.IO.ANDI.Function import ANDI_reader
 from pyms.GCMS.Function import build_intensity_matrix
-from pyms.Noise.Window import window_smooth_im
+from pyms.Noise.SavitzkyGolay import savitzky_golay_im
 
 # read the raw data
 andi_file = "/x/PyMS/data/gc01_0812_066.cdf"
@@ -15,9 +15,9 @@ data = ANDI_reader(andi_file)
 # build an intensity matrix object from the data
 im = build_intensity_matrix(data)
 
-# Use window averaging to smooth all IC's in the IM
+# Use Savitzky-Golay filtering to smooth all IC's in the IM
 print "Smoothing ..."
-im_smooth = window_smooth_im(im, window=5, median=False)
+im_smooth = savitzky_golay_im(im)
 print "Done"
 
 # find the IC for derivatisation product ion before smoothing
