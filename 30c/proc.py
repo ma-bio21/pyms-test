954c8a4574613c82188bf7be9aa3554bb0cb8cf3
v.likic <v.likic@7eee158e-254d-0410-9f01-bbecc1f07e93>
2011-01-13 00:37:20 +0000

    Cosmetic improvements

--- 30c/proc.py
+++ 30c/proc.py
@@ -16,14 +16,16 @@ data = JCAMP_reader(jcamp_file)
 
 # integer intensity matrix, integer masses, in one unit steps
 print "intensity matrix with integer mass and bin interval = 1, "\
-    "default boundary -0.3, +0.7"
+    "using default boundary -0.3, +0.7"
 im = build_intensity_matrix_i(data)
 
-print "size of intensity matrix (#scans, #bins):", im.get_size()
+print " -> size of intensity matrix (#scans, #bins):", im.get_size()
 
-print "start mass:", im.get_min_mass()
-print "end mass:", im.get_max_mass()
+print " -> start mass:", im.get_min_mass()
+print " -> end mass:", im.get_max_mass()
 
 index = im.get_index_of_mass(73.3)
-print "the index of the nearest mass to 73.3m/z is:", index
-print "the nearest mass to 73.3m/z is:", im.get_mass_at_index(index)
+
+print " -> the index of the nearest mass to 73.3 m/z is:", index
+print " -> the nearest mass to 73.3 m/z is:", im.get_mass_at_index(index)
+
